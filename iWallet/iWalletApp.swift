7912a0f0037fb70c4da91b52b4c0b401032f094f
ilyakozhevn <kozhevilya@gmail.com>
2022-06-07 08:00:56 +0300

    Initial Commit

---
+++ iWallet/iWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWalletApp.swift
+//  iWallet
+//
+//  Created by Ilya Kozhevnikov on 07.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct iWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
